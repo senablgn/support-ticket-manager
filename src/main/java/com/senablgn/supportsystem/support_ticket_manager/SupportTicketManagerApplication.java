e554d49dd2c1fd9a8911408cf31954ff418ad232
Sena Bilgin <senabilgin17@gmail.com>
2025-07-31 20:31:55 +0300

    initial commit

---
+++ src/main/java/com/senablgn/supportsystem/support_ticket_manager/SupportTicketManagerApplication.java
@@ -0,0 +1,13 @@
+package com.senablgn.supportsystem.support_ticket_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTicketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportTicketManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senablgn/supportsystem/support_ticket_manager/SupportTicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senablgn.supportsystem.support_ticket_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportTicketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
