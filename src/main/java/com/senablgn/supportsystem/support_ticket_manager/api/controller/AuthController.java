14132c7636038ecb288cc7859cdaea453056d597
Sena Bilgin <senabilgin17@gmail.com>
2025-08-02 20:21:24 +0300

    logout controller eklendi

--- src/main/java/com/senablgn/supportsystem/support_ticket_manager/api/controller/AuthController.java
+++ src/main/java/com/senablgn/supportsystem/support_ticket_manager/api/controller/AuthController.java
@@ -1,17 +1,18 @@
 package com.senablgn.supportsystem.support_ticket_manager.api.controller;
 
+import com.senablgn.supportsystem.support_ticket_manager.core.utilities.results.Result;
+import com.senablgn.supportsystem.support_ticket_manager.core.utilities.results.SuccessResult;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import com.senablgn.supportsystem.support_ticket_manager.business.abstracts.AuthService;
 import com.senablgn.supportsystem.support_ticket_manager.core.utilities.results.DataResult;
 import com.senablgn.supportsystem.support_ticket_manager.core.utilities.results.SuccessDataResult;
 import com.senablgn.supportsystem.support_ticket_manager.dto.request.AuthRequest;
 import com.senablgn.supportsystem.support_ticket_manager.dto.request.CreateUserRequest;
 import com.senablgn.supportsystem.support_ticket_manager.dto.request.RefreshTokenRequest;
+
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,15 +21,25 @@ public class AuthController {
 	private AuthService authService;
 
 	@PostMapping("/register")
-	public ResponseEntity<DataResult>register(@RequestBody CreateUserRequest createUserRequest) {
-		return ResponseEntity.ok(new SuccessDataResult("user registered", authService.register(createUserRequest)));
+	public ResponseEntity<DataResult> register(@RequestBody CreateUserRequest createUserRequest) {
+		return ResponseEntity
+				.ok(new SuccessDataResult("user registered", this.authService.register(createUserRequest)));
 	}
+
 	@PostMapping("/login")
-	public ResponseEntity<DataResult>login(@RequestBody AuthRequest authRequest) {
-		return ResponseEntity.ok(new SuccessDataResult("user login", authService.login(authRequest)));
+	public ResponseEntity<DataResult> login(@RequestBody AuthRequest authRequest) {
+		return ResponseEntity.ok(new SuccessDataResult("user login", this.authService.login(authRequest)));
 	}
+
 	@PostMapping("/refreshToken")
-	public ResponseEntity<DataResult>refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
-		return ResponseEntity.ok(new SuccessDataResult("user refresh token", authService.refreshToken(refreshTokenRequest)));
+	public ResponseEntity<DataResult> refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+		return ResponseEntity
+				.ok(new SuccessDataResult("user refresh token", this.authService.refreshToken(refreshTokenRequest)));
+	}
+
+	@PostMapping("/logout")
+	public ResponseEntity<Result> logout(@RequestHeader(value = "Authorization") String header) {
+		this.authService.logout(header);
+		return ResponseEntity.ok(new SuccessResult("user logged out"));
 	}
 }
