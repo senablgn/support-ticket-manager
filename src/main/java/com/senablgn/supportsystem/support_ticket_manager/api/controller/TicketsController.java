c97792328d3578e381cb51871d1c257482c12030
Sena Bilgin <senabilgin17@gmail.com>
2025-08-03 00:22:07 +0300

    ticket listeleme admin yetkisi eklendi

--- src/main/java/com/senablgn/supportsystem/support_ticket_manager/api/controller/TicketsController.java
+++ src/main/java/com/senablgn/supportsystem/support_ticket_manager/api/controller/TicketsController.java
@@ -8,6 +8,7 @@ import com.senablgn.supportsystem.support_ticket_manager.dto.response.TicketResp
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class TicketsController {
 	private TicketService ticketService;
 
 	@GetMapping
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<DataResult<List<TicketResponse>>>getAllTickets(){
 		return ResponseEntity.ok(new SuccessDataResult("tickets", this.ticketService.getAllTickets()));
 	}
