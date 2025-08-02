d2f471e524d5ed7def5b863fe4fe4e467398c9ac
Sena Bilgin <senabilgin17@gmail.com>
2025-08-02 16:19:16 +0300

    auth response ve request sınıfları eklendi

---
+++ src/main/java/com/senablgn/supportsystem/support_ticket_manager/dto/response/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.senablgn.supportsystem.support_ticket_manager.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+	private String accessToken;
+}
