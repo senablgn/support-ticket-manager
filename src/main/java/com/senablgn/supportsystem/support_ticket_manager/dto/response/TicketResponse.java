005e64bbc49a27d6dd01f39f6b8cf8e6c0689698
Sena Bilgin <senabilgin17@gmail.com>
2025-08-03 00:14:55 +0300

    get all tickets controller metodu eklendi

--- src/main/java/com/senablgn/supportsystem/support_ticket_manager/dto/response/TicketResponse.java
+++ src/main/java/com/senablgn/supportsystem/support_ticket_manager/dto/response/TicketResponse.java
@@ -3,13 +3,19 @@ package com.senablgn.supportsystem.support_ticket_manager.dto.response;
 import com.senablgn.supportsystem.support_ticket_manager.entities.TicketStatus;
 import com.senablgn.supportsystem.support_ticket_manager.entities.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TicketResponse {
 	private String subject;
 	private String description;
 	private TicketStatus status;
 	private LocalDateTime createdAt;
-	private User user;
+	private Long userId;
+	private String username;
 }
--- src/main/java/com/senablgn/supportsystem/support_ticket_manager/entities/TicketStatus.java
+++ src/main/java/com/senablgn/supportsystem/support_ticket_manager/entities/TicketStatus.java
@@ -1,10 +1,5 @@
 package com.senablgn.supportsystem.support_ticket_manager.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
 public enum TicketStatus {
 	PENDING, RESPONDED, CLOSED;
 }
