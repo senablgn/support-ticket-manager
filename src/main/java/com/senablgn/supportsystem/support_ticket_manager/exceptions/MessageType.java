0e2c2017b744ac84832d2aff713326960ca1c56a
Sena Bilgin <senabilgin17@gmail.com>
2025-08-02 20:22:40 +0300

    error mesajları güncellendi

--- src/main/java/com/senablgn/supportsystem/support_ticket_manager/exceptions/MessageType.java
+++ src/main/java/com/senablgn/supportsystem/support_ticket_manager/exceptions/MessageType.java
@@ -11,7 +11,8 @@ public enum MessageType {
 	GENERAL_ERROR("1","General error has occured"),
 	TOKEN_NOT_FOUND("2","Token not found"),
 	MISSING_TOKEN("3","Token is missing"),
-	INVALID_TOKEN("4","Token is already invalid");
+	INVALID_TOKEN("4","Token is already invalid"),
+	USER_NOT_FOUND("5","User not found");
 	private String code;
 	private String message;
 }
