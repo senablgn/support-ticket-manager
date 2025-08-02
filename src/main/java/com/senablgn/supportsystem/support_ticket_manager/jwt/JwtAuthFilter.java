b01b5bdc58b773f5689bdda696bce13dd6c33e08
Sena Bilgin <senabilgin17@gmail.com>
2025-08-02 20:28:11 +0300

    filter    eter black list token için güncellendi

--- src/main/java/com/senablgn/supportsystem/support_ticket_manager/jwt/JwtAuthFilter.java
+++ src/main/java/com/senablgn/supportsystem/support_ticket_manager/jwt/JwtAuthFilter.java
@@ -1,5 +1,6 @@
 package com.senablgn.supportsystem.support_ticket_manager.jwt;
 
+import com.senablgn.supportsystem.support_ticket_manager.dataAccess.BlackListedTokenRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,6 +21,7 @@ import java.io.IOException;
 public class JwtAuthFilter extends OncePerRequestFilter {
 	private JwtUtil jwtUtil;
 	private UserDetailsService userDetailsService;
+	private BlackListedTokenRepository blackListedTokenRepository;
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
@@ -29,6 +31,10 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 		if (header != null && header.startsWith("Bearer ")) {
 			jwtTokern = header.substring(7);
 			username = jwtUtil.extractUsername(jwtTokern);
+		}
+		if(this.blackListedTokenRepository.existsByToken(jwtTokern)) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return;
 		}
 		if(username!=null && SecurityContextHolder.getContext().getAuthentication()==null) {
 			UserDetails userDetails = userDetailsService.loadUserByUsername(username);
